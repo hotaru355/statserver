8f14a905a5200a6adc2522751f7020e191383526
Kenta Fried <kenta.fried@gmail.com>
2013-11-26 22:42:38 -0800

    added raw json form for requests

--- src/main/resources/html/js/statserver.js
+++ src/main/resources/html/js/statserver.js
@@ -3,10 +3,44 @@
 	// jQuery UI hookup and click handlers
 	$(function() {
 		$('#tabs').tabs();
-		$('#requestLeaderboard').button().click(loadLeaderboard);
-		$('#requestStats').button().click(loadStats);
-		$('#postStat').button().click(postStat);
-		$('#postCreatedAt').datetimepicker();
+
+		// click handler to POST a stat
+		$('#postStatGuided').button().click(function() {
+			var stat = JSON.stringify({
+				statName : $('#tabSendStat .request .statName option:selected').val(),
+				value : $('#tabSendStat .request .value').val(),
+				createdAt : new Date($('#tabSendStat .request .createdAt').val()).getTime()
+			});
+			var url = getBaseUrl() + '?userName=' + $('#tabSendStat .request .userName').val();
+			postStat(stat, url, fillGuidedPost);
+		});
+		$('#postStatRaw').button().click(function() {
+			var stat = $('#tabSendStat .rawJson').val();
+			var url = getBaseUrl() + '?' + $('#tabSendStat .queryParam').val();
+			postStat(stat, url, fillRawPost);
+		});
+
+		// click handler to GET a leaderboard
+		$('#getLeaderboardGuided').button().click(function() {
+			var url = getBaseUrl() + '/leaderboard?statName=' + $('#tabGetLeaderboard .statName option:selected').val();
+			loadLeaderboard(url, fillGuidedLeaderboard);
+		});
+		$('#getLeaderboardRaw').button().click(function() {
+			var url = getBaseUrl() + '/leaderboard?' + $('#tabGetLeaderboard .queryParam').val();
+			loadLeaderboard(url, fillRawLeaderboard);
+		});
+
+		// click handler to GET a stat
+		$('#getStatsGuided').button().click(function() {
+			var url = getBaseUrl() + '?userName=' + $('#tabGetStat .userName').val();
+			loadStats(url, fillGuidedGet);
+		});
+		$('#getStatsRaw').button().click(function() {
+			var url = getBaseUrl() + '?' + $('#tabGetStat .queryParam').val();
+			loadStats(url, fillRawGet);
+		});
+
+		$('#tabSendStat .request .createdAt').datetimepicker();
 
 		if (window.location.hostname) {
 			$('#baseUrl').val('http://' + window.location.hostname);
@@ -16,12 +50,9 @@
 	});
 
 	// display AJAX errors in div
-	$(document).ajaxError(
-			function(e, xhr, settings, exception) {
-				$('#ajaxError').text(
-						'Error in: ' + settings.url + ' - Error: ' + exception
-								+ " - Response: " + xhr.responseText);
-			});
+	$(document).ajaxError(function(e, xhr, settings, exception) {
+		$('#ajaxError').text('Error in: ' + settings.url + ' - Error: ' + exception + " - Response: " + xhr.responseText);
+	});
 
 	// retrieve the base REST URL
 	function getBaseUrl() {
@@ -30,10 +61,7 @@
 	}
 
 	// load the leaderboard from the back end
-	function loadLeaderboard() {
-		var url = getBaseUrl() + '/leaderboard?statName='
-				+ $('#statName option:selected').val();
-
+	function loadLeaderboard(url, successFunc) {
 		$.ajax({
 			cache : false,
 			async : false,
@@ -49,22 +77,25 @@
 				$('input:button').show();
 			},
 			success : function(responseJson) {
-				$("#leaderboardTable").find("tr:gt(0)").remove();
-				for (var i = 0, len = responseJson.length; i < len; i++) {
-					$('#leaderboardTable tr:last').after(
-							'<tr><td>' + responseJson[i]['rank'] + '</td><td>'
-									+ responseJson[i]['userName'] + '</td><td>'
-									+ responseJson[i]['value'] + '</td><tr>');
-
-				}
+				successFunc(responseJson);
 			}
 		});
+	}
+
+	function fillGuidedLeaderboard(json) {
+		$("#leaderboardTable").find("tr").remove();
+		for (var i = 0, len = json.length; i < len; i++) {
+			$('#leaderboardTable').append(
+					'<tr><td>' + json[i]['rank'] + '</td><td>' + json[i]['userName'] + '</td><td>' + json[i]['value'] + '</td><tr>');
+		}
+	}
+
+	function fillRawLeaderboard(json) {
+		$('#tabGetLeaderboard .raw .response .rawJson').html(JSON.stringify(json));
 	}
 
 	// load the stats from the back end
-	function loadStats() {
-		var url = getBaseUrl() + '?userName=' + $('#userName').val();
-
+	function loadStats(url, successFunc) {
 		$.ajax({
 			cache : false,
 			async : false,
@@ -80,37 +111,26 @@
 				$('input:button').show();
 			},
 			success : function(responseJson) {
-				$("#statTable").find("tr:gt(0)").remove();
-				for (var i = 0, len = responseJson.length; i < len; i++) {
-					var date = new Date(responseJson[i]['createdAt'] * 1000);
-					var formattedTime = padTo2Digit(date.getHours()) + ':'
-							+ padTo2Digit(date.getMinutes()) + ':'
-							+ padTo2Digit(date.getSeconds());
-					$('#statTable tr:last')
-							.after(
-									'<tr><td>' + responseJson[i]['statName']
-											+ '</td><td>'
-											+ responseJson[i]['value']
-											+ '</td><td>' + formattedTime
-											+ '</td><tr>');
-
-				}
-				;
+				successFunc(responseJson);
 			}
 		});
 	}
 
+	function fillGuidedGet(json) {
+		$("#statsTable").find("tr").remove();
+		for (var i = 0, len = json.length; i < len; i++) {
+			var date = new Date(json[i]['createdAt'] * 1000);
+			var formattedTime = padTo2Digit(date.getHours()) + ':' + padTo2Digit(date.getMinutes()) + ':' + padTo2Digit(date.getSeconds());
+			$('#statsTable').append('<tr><td>' + json[i]['statName'] + '</td><td>' + json[i]['value'] + '</td><td>' + formattedTime + '</td><tr>');
+		}
+	}
+
+	function fillRawGet(json) {
+		$('#tabGetStat .rawJson').html(JSON.stringify(json));
+	}
+
 	// post a new stat to the back end
-	// TODO fix me
-	function postStat() {
-		var url = getBaseUrl() + '?userName=' + $('#postUserName').val();
-
-		var stat = JSON.stringify({
-			statName : $('#postStatName option:selected').val(),
-			value : $('#postValue').val(),
-			createdAt : new Date($('#postCreatedAt').val()).getTime()
-		});
-
+	function postStat(stat, url, successFunc) {
 		$.ajax({
 			cache : false,
 			async : false,
@@ -127,9 +147,21 @@
 				$('input:button').show();
 			},
 			success : function(responseJson) {
-				$('#postResponse').html(JSON.stringify(responseJson));
+				successFunc(responseJson);
 			}
 		});
+	}
+
+	function fillGuidedPost(json) {
+		$('#tabSendStat .response .id').val(json["id"]);
+		$('#tabSendStat .response .userName').val(json["userName"]);
+		$('#tabSendStat .response .statName').val(json["statName"]);
+		$('#tabSendStat .response .value').val(json["value"]);
+		$('#tabSendStat .response .createdAt').val(new Date(json["createdAt"] * 1000));
+	}
+
+	function fillRawPost(json) {
+		$('#tabSendStat .response .rawJson').html(JSON.stringify(json));
 	}
 
 	// pads single digit numbers with a zero
